98f92d6a4b9ff5f1cd77756a3a2b19936781915a
SINOVIA <somayfield@gmail.com>
2016-06-09 09:41:41 -0400

    Pre Submission Push

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,13 +30,6 @@ $(function () {
   //     document.user-form.submit();
   //   }
 
-  function showError(req, status, err) {
-    err = err || {};
-    err.message = err.message || status;
-    console.log(err);
-    show('gh-error-template', { message: err });
-  }
-
   function show(template, model) {
     var fn = _.template($('#' + template).html(), { variable: 'm' });
     $('.user-info').html(fn(model));
